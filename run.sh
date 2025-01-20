9ab158b82ad71b18a6aef8fc5345d5fd1f8d3adb
Andrey Markov <markov_av@atb.su>
2025-01-20 22:51:18 +1000

    fesfsef

--- run.sh
+++ run.sh
@@ -4,13 +4,13 @@ cmake ..
 cmake --build .
 
 echo "Running lab1"
-./Integration > lab1.txt
+./Integration > ../lab1.txt
 
 echo "Running lab2-3"
-./Matrix > lab2-3.txt
+./Matrix > ../lab2-3.txt
 
 echo "Running lab4"
-./ParallelMod > lab4.txt
+./ParallelMod > ../lab4.txt
 
 echo "Running lab5"
-./FFT > lab5.txt
+./FFT > ../lab5.txt
